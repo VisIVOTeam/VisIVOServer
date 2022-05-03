8c49e6b873eec98e1eb9d414760cc94a8607fac3
Fabio Vitello <fvitello@users.noreply.github.com>
2022-05-03 08:24:06 +0200

    First Version of importer for unstructured grid Point Data

--- src/VisIVOImporter/Importers/vtksource.cpp
+++ src/VisIVOImporter/Importers/vtksource.cpp
@@ -81,29 +81,19 @@ int VTKSource::readData()
         std::cout << data->GetNumberOfPieces() << std::endl;
         double bounds[6];
         data->GetBounds(bounds);
-        std::cout << bounds[0] << " " << bounds[1] << " " << bounds[2] << " " << bounds[3] << " " << bounds[4] << " " << bounds[5] << std::endl;
         
         double center[3];
         data->GetCenter(center);
-        std::cout << center[0] << " " << center[1] << " " << center[2] << std::endl;
-        
-        std::cout << data->GetPointData()[0].GetNumberOfArrays() << std::endl;
-        std::cout << data->GetPointData()[0].GetScalars()[0] << std::endl;
-        std::cout << data->GetPointData()[0].GetVectors()[0] << std::endl;
         
         // Now check for point data
         vtkPointData *pd = data->GetPointData();
         if (pd)
         {
-            std::cout << " contains point data with " << pd->GetNumberOfArrays() << " arrays."             << std::endl;
             std::string suffix;
             
-            for (int i = 1; i < 2; i++)
+            for (int i = 0; i < pd->GetNumberOfArrays() ; i++)
             {
                 suffix="";
-                std::cout << "\tArray " << i << " is named " << (pd->GetArrayName(i) ? pd->GetArrayName(i) : "NULL") << " #ofcomp: "<<pd->GetAbstractArray(i)->GetNumberOfComponents()<<std::endl;
-                
-                
                 
                 for (int j = 0; j < pd->GetAbstractArray(i)->GetNumberOfComponents(); j++)
                 {
@@ -111,53 +101,44 @@ int VTKSource::readData()
                         suffix=":"+ std::to_string(j);
                     
                     m_fieldNames.push_back(pd->GetArrayName(i)+suffix);
-                    std::cout << "\t\t scrivo Array " << pd->GetArrayName(i)+suffix << " di dim " << pd->GetAbstractArray(i)->GetNumberOfTuples()<<std::endl;
                     
-                    /*
-                     for (vtkIdType tupleIdx = 0; tupleIdx < pd->GetAbstractArray(i)->GetNumberOfTuples(); ++tupleIdx)
-                     {
-                     std::cout << pd->GetArray(i)->GetComponent(tupleIdx, j) << std::endl;
-                     
-                     // outFile.write((char *)(std::to_string(pd->GetArray(i)->GetComponent(tupleIdx, j)).c_str()), 1);
-                     
-                     }
-                     */
-                    std::cout <<pd->GetAbstractArray(i)->GetNumberOfTuples()<<std::endl;
-                    outFile.write((char *)(vtkFloatArray::SafeDownCast(pd->GetAbstractArray(i))->GetPointer(0)), sizeof(float)*pd->GetAbstractArray(i)->GetNumberOfTuples());//sizeof(vtkIntArray) * pd->GetAbstractArray(i)->GetNumberOfTuples());
+                    vtkNew<vtkFloatArray> tmp;
+                    tmp->SetNumberOfComponents(1);
+                    tmp->SetNumberOfTuples(pd->GetAbstractArray(i)->GetNumberOfTuples());
+                    tmp->CopyComponent(0, pd->GetArray(i), j);
                     
+                    outFile.write((char *)(tmp->GetPointer(0)), sizeof(float)*pd->GetAbstractArray(i)->GetNumberOfTuples());
                     
                 }
+                
             }
             
-            
-            
-            
         }
-        /*
-         // Now check for cell data
-         vtkCellData *cd = data->GetCellData();
-         if (cd)
-         {
-         std::cout << " contains cell data with " << cd->GetNumberOfArrays() << " arrays."
-         << std::endl;
-         for (int i = 0; i < cd->GetNumberOfArrays(); i++)
-         {
-         std::cout << "\tArray " << i << " is named "
-         << (cd->GetArrayName(i) ? cd->GetArrayName(i) : "NULL") << std::endl;
-         }
-         }
-         // Now check for field data
-         if (data->GetFieldData())
-         {
-         std::cout << " contains field data with " << data->GetFieldData()->GetNumberOfArrays()
-         << " arrays." << std::endl;
-         for (int i = 0; i < data->GetFieldData()->GetNumberOfArrays(); i++)
-         {
-         std::cout << "\tArray " << i << " is named "
-         << data->GetFieldData()->GetArray(i)->GetName() << std::endl;
-         }
-         }
-         */
+        
+        // Now check for cell data
+        vtkCellData *cd = data->GetCellData();
+        if (cd)
+        {
+            std::cout << " contains cell data with " << cd->GetNumberOfArrays() << " arrays."
+            << std::endl;
+            for (int i = 0; i < cd->GetNumberOfArrays(); i++)
+            {
+                std::cout << "\tArray " << i << " is named "
+                << (cd->GetArrayName(i) ? cd->GetArrayName(i) : "NULL") << std::endl;
+            }
+        }
+        // Now check for field data
+        if (data->GetFieldData())
+        {
+            std::cout << " contains field data with " << data->GetFieldData()->GetNumberOfArrays()
+            << " arrays." << std::endl;
+            for (int i = 0; i < data->GetFieldData()->GetNumberOfArrays(); i++)
+            {
+                std::cout << "\tArray " << i << " is named "
+                << data->GetFieldData()->GetArray(i)->GetName() << std::endl;
+            }
+        }
+        
     }
     else if (type == rectilinear_grid)
     {
@@ -173,15 +154,12 @@ int VTKSource::readData()
         std::cout << data->GetNumberOfCells() << std::endl;
     }
     
-    // std::clog<<m_pointsBinaryName.c_str()<<endl;
-    //  Read Data
     
     if (!outFile)
         return 1;
     outFile.close();
     
     makeHeader(m_nRows, m_pointsBinaryName, m_fieldNames, m_cellSize, m_cellComp, m_volumeOrTable);
-    
     
     return 1;
 }
